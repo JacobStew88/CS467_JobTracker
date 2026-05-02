55facf51bf394905fc1bcd53b27133da5f476f58
Francis Truong <frankisthetruong@gmail.com>
2026-05-02 13:07:00 -0700

    Added Routes for jobs / new file for user

---
+++ backend/src/routes/skillRoutes.ts
@@ -0,0 +1,15 @@
+import * as skillController from '../controllers/skillController';
+import { Router } from 'express';
+
+const skillRouter = Router();
+
+skillRouter.post('/', skillController.createSkillController);
+skillRouter.get('/', skillController.getSkillsController);
+skillRouter.get('/:id', skillController.getSkillController);
+skillRouter.put('/:id', skillController.updateSkillController);
+skillRouter.delete('/:id', skillController.deleteSkillController);
+skillRouter.post('/:id/jobs/:jobid', skillController.assignSkillToJobController);
+skillRouter.delete('/:id/jobs/:jobid', skillController.removeSkillFromJobController);
+skillRouter.get('/jobs/:jobid', skillController.getSkillsFromJobController);
+
+export default skillRouter;
