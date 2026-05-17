0670da7bee92420ddd60bdc92dce19e626702ce3
Jacob Stewart <Jacob.stew88@gmail.com>
2026-05-17 11:41:37 -0600

    added a login vs logout home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,36 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
-import Hero from "../components/Hero"; 
+import Hero from "../components/Hero";
+import { AuthContext } from "../components/AuthContext";
 
 export default function Home() {
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
+
   return (
     <>
-    <Hero />
-    <div className="body">
-      <div className="button-group">
-      <Button onClick={() => navigate("/login")}>
-        Sign in
-      </Button>
+      <Hero />
 
-      <Button onClick={() => navigate("/signup")}>
-        Sign up
-      </Button>
+      <div className="body">
+        <div className="button-group">
+          {user ? (
+            <Button onClick={() => navigate("/jobs")}>
+              Go to Jobs
+            </Button>
+          ) : (
+            <>
+              <Button onClick={() => navigate("/login")}>
+                Sign in
+              </Button>
+
+              <Button onClick={() => navigate("/signup")}>
+                Sign up
+              </Button>
+            </>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 }
